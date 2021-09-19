349e24299dbabb09924f161fc933feca1b95ea49
Buchi Reddy B <buchi@levo.ai>
2021-09-19 00:43:00 -0700

    Adding a fat container for crAPI so that we can run crAPI quickly.
    docker build -t crapi/crapi-all .
    docker run -p 80:80 crapi/crapi-all

---
+++ crAPI/start_all_services.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+waitport() {
+  while ! nc -z localhost $1 ; do sleep 0.3 ; done
+}
+
+wait_endpoint() {
+  while ! echo exit | curl -s $1; do sleep 3; done
+}
+
+# Start postgres
+/usr/local/bin/docker-entrypoint.sh postgres &
+
+# Wait for Postgres to come up fully
+waitport 5432
+
+# Start MongoDB
+/usr/bin/mongod -f /etc/mongod.conf --auth --fork --quiet --logpath /var/log/mongodb.log --logappend
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb.log
+while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+    grep -q 'waiting for connections on port' /var/log/mongodb.log
+done
+
+# Initialize the MongoDB with init users.
+mongo --authenticationDatabase admin <<EOF
+use admin;
+db.createUser({user: 'admin', pwd: 'crapisecretpassword', roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]})
+EOF
+
+# Start mailhog after MongoDB
+/usr/local/bin/MailHog &
+
+# Wait for MailHog to come up
+waitport 8025
+# while ! echo exit | nc localhost 8025; do sleep 1; done
+
+# Start identity service
+java -jar /app/identity/user-microservices-1.0-SNAPSHOT.jar &
+
+# Wait for identity service to fully come up
+wait_endpoint 0.0.0.0:8080/identity/health_check
+
+# Start community service
+/app/community/main &
+
+# Wait for community service to fully come up
+wait_endpoint 0.0.0.0:8087/community/home
+
+# Start workshop service
+cd "$(dirname "${BASH_SOURCE[0]}")/workshop"
+sh ./runner.sh &
+
+# Wait for workshop service to fully come up
+wait_endpoint 0.0.0.0:8000/workshop/health_check/
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Start Nginx to start web at the end
+/etc/nginx/nginx-wrapper.sh
