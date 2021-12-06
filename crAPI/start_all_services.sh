463358b9132d66b9e1d7801bf15542b6cb40aa47
Buchi Reddy B <buchi@levo.ai>
2021-12-05 20:42:25 -0800

    Removing debug information

--- crAPI/start_all_services.sh
+++ crAPI/start_all_services.sh
@@ -34,8 +34,6 @@ while [[ $? -ne 0 && $COUNTER -lt 10 ]] ; do
     let COUNTER+=1
     echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
     grep -q 'waiting for connections on port' /var/log/mongodb.log
-    ls /var/log/mongodb.log
-    cat /var/log/mongodb.log
 done
 
 # Initialize the MongoDB with init users.
